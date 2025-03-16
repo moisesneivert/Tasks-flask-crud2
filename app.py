e03301478e8c91380c51de9e4634859774a6ad65
moisesneivert <143449564+moisesneivert@users.noreply.github.com>
2025-03-16 18:37:04 -0300

    Tasks-flask-crud2 Atualizado

--- app.py
+++ app.py
@@ -14,15 +14,11 @@ task_id_control = 1
 def create_task():
     global task_id_control
     data = request.get_json()
-    
-    
     new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
     task_id_control += 1
     tasks.append(new_task)
-    
     print(tasks)
-    
-    return jsonify({"message": "Nova tarefa criada com sucesso"}), 201  # Retorna status 201 Created
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id}), 201  # Retorna status 201 Created
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
